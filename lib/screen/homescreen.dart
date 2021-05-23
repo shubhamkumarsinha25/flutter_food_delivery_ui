89117c21c2c79ad6b8678fa504ab5838db048790
shubhamkumarsinha25 <shubhamkumarsinha25@gmail.com>
2021-05-23 13:25:06 +0530

    Upload

---
+++ lib/screen/homescreen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/widget/recent_orders.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  _buildRestrones(){
+    List <Widget>restroneslist=[];
+    return Column()
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.account_circle,
+          ),
+          iconSize: 30,
+          onPressed: () {},
+        ),
+        title: Center(
+          child: Text('Food Delivery'),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Cart(${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.grey[200],
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search Food and Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear, size: 30.0),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+          RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'NEARBY RESTRONE',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+              _buildRestrones(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
